68f60da13c4f2093d9716e76de157313f66277ac
Andre-Weber <andreew@al.insper.edu.br>
2021-12-17 02:17:40 -0300

    Updates

--- scripts/commonYo.js
+++ scripts/commonYo.js
@@ -81,7 +81,7 @@ let checkCurrentUser = function () {
 }
 
 function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 
 
